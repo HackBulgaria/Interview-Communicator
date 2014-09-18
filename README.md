c1da50bb3624923a152a6283b85b86780819a5b0
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2014-09-18 13:26:44 +0300

    Adding basic intructions & script for mail sending

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ Interview-Communicator
 ======================
 
 A set of rules and scripts for making interviews for Hack Bulgaria's courses
+
+## Steps for making interview schedule with studensts.
+
+1. Download all people from F6S as a CSV file.
+2. Import that CSV file into a Google Documents Spreadsheet
+3. Trim the data and make it have 4 columns:
+    3.1 Name
+    3.2 Email
+    3.3 Date
+    3.4 Hour
+4. Export the data as CSV file again.
+5. Transform it to a JSON file (You can do that with http://www.convertcsv.com/csv-to-json.htm)
+6. Save the file as `people.json`
+7. Run the script `send_emails.py` - requires Python v. > 3
+
+## Detailed explanation
---
+++ send_emails.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import smtplib
+import json
+import time
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+EMAIL = 'ivaylo@hackbulgaria.com'
+PASSWORD = "YOUR-PASSWORD-HERE!"
+SMPT_SERVER = 'giga.superhosting.bg'
+SMPT_SERVER_PORT = 26
+
+
+json_data = open('people.json')
+people = json.load(json_data)
+
+server = smtplib.SMTP(SMPT_SERVER, SMPT_SERVER_PORT)
+server.ehlo()
+server.starttls()
+server.login(EMAIL, PASSWORD)
+
+for person in people:
+    html = u"""\
+        <html>
+          <head></head>
+          <body>
+            <p>Здравей , {}</p>
+
+            <p>Дойде време и за интервютата за Programing101 - курса към Хак България.</p>
+            <p>
+                <strong>
+                    Датата и часът за твоето интервю са: {} - {}h <br>
+                    Молбата ни е да потвърдиш на този email, ако си ОК с датата и часът.
+                </strong>
+            </p>
+
+            <p>Подготвили сме ти малко материали, които да прочетеш и решиш преди началото на курса: <a href="https://hackbulgaria.com/course/Prog101-2/#pre-reading">https://hackbulgaria.com/course/Prog101-2/#pre-reading</a>
+            </p>
+            Важното за интервютата е:
+            <ul>
+                <li>Интервюто ще се проведе по Skype</li>
+                <li>Продължителността на едно интервю е 15 минути</li>
+                <li>Skype акаунтът, който искаш да намериш е: hackbulgaria</li>
+                <li>Ще си поговорим с теб, за да разберем колко си мотивиран-а и до къде се простират знанията ти в областта на програмирането.</li>
+            </ul>
+            <p>
+                Ако не използваш Skype ще се договорим за друга платформа.
+            </p>
+            <p>
+                Поздрави,<br>
+                Екипа на HackBulgaria
+            </p>
+          </body>
+        </html>
+        """.format(person['Name'], person['Date'], person['Hour'])
+
+    me = "ivaylo@hackbulgaria.com"
+    you = person['Email']
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "HackBulgaria дата и час за интервю."
+    msg['From'] = me
+    msg['To'] = you
+
+    part1 = MIMEText(html, 'html')
+    msg.attach(part1)
+
+    server.sendmail(me, you, msg.as_string())
+    print("Send: " + you)
+
+    time.sleep(4)
+
+server.quit()
